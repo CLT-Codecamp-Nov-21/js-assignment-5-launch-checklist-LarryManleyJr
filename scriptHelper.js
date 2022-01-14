97f66f10f17ec282f108bc0c310a9ded232f35ea
Notorious Nerd <NotoriousNerd@The-11-Creative.local>
2022-01-14 13:52:37 -0500

    still not working

--- scriptHelper.js
+++ scriptHelper.js
@@ -1,5 +1,10 @@
 // Write your helper functions here!
+try {
 require('isomorphic-fetch');
+} catch(e) {
+    //do nothing
+}
+
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
@@ -17,7 +22,12 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   let pilotNameInput = document.forms["testform"]["pilotName"].value;
+   let copilotNameInput = document.querySelector("input[text=copilotName]");
+   if (pilotNameInput == ""){
+       alert("Name must be filled out");
+       return false;
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
@@ -36,8 +46,12 @@ async function myFetch() {
 function pickPlanet(planets) {
 }
 
+try {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
 module.exports.pickPlanet = pickPlanet; 
 module.exports.myFetch = myFetch;
+} catch (e) {
+    //do nothing
+}
